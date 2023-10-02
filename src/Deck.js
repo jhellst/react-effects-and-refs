6bc4174c1ab8487144326ce0298c9cbd091452cb
Michael <michaellngriffin@gmail.com>
2023-10-02 02:45:53 -0400

    first code review done, shuffle needed

--- src/Deck.js
+++ src/Deck.js
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from "react";
 
-const NEW_DECK_URL = "https://deckofcardsapi.com/api/deck/new/";
-const DRAW_CARD_URL = "https://deckofcardsapi.com/api/deck/new/";
+const BASE_URL = "https://deckofcardsapi.com/api/deck";
 
+
+
+/** Pulls a deck from CardsAPI, then draw a card from API and display in list */
 function Deck() {
   const [deck, setDeck] = useState({
     deck: null,
@@ -11,33 +13,63 @@ function Deck() {
 
   const [cardsDrawn, setCardsDrawn] = useState([]);
 
-  // Possibly another state variable that holds pile of drawn cards
+
   useEffect(function fetchAndSetDeck() {
     async function fetchDeck() {
-      const response = await fetch(NEW_DECK_URL);
+      const response = await fetch(`${BASE_URL}/new/`);
       const deck = await response.json();
       setDeck({
         deck: deck,
         loaded: true
       });
     }
+    fetchDeck();
   }, []);
 
+
+
+  /** Draws a single card */
   async function drawCard() {
-    const response = await fetch(DRAW_CARD_URL);
+    const response = await fetch(`${BASE_URL}/${deck.deck.deck_id}/draw/?count=1`);
     const cardInfo = await response.json();
+    //console.log(cardInfo)
     const card = cardInfo.cards[0];
-    setCardsDrawn(prevCards => card);
+      //card.style = (our style stuff)
+    setCardsDrawn(prevCards => [...prevCards, card]);
+  }
+
+
+
+  function makeCardList(){
+    const jitter = 20;
+
+    const cardPile = cardsDrawn.map(card => {
+      let xJitter = Math.round(jitter*Math.random());
+      let yJitter = Math.round(jitter*Math.random());
+      let currAngle = Math.round(360*Math.random());
+
+      const imgStyle = {
+        position: "absolute",
+        transform: `rotate(${currAngle}deg)`,
+        top: `${180 + yJitter}px`,
+        left: `${180 + xJitter}px`,
+      }
+      return <img src={card.image} style={imgStyle} />;
+    });
+    return cardPile;
   }
 
   return (
     <div>
-      {(deck.loaded) && <button onClick={drawCard}></button>}
-      {(cardsDrawn.length > 0) && <CardsPile />}
+      <h1>Card Display</h1>
+      {(deck.loaded) && <button onClick={drawCard}>Draw Card</button>}
+      <br />
+      {(cardsDrawn.length > 0) &&
+      <div className="CardPile">
+        {makeCardList()}
+      </div>}
     </div>
   );
-
-
 }
 
 export default Deck;
@@ -69,3 +101,20 @@ export default Deck;
 //   ],
 //   "remaining": 50
 // }
+
+
+// function handleClick(evt) {
+//   const x = window.innerWidth * Math.random();
+//   const y = window.innerHeight * Math.random();
+//   setBags(prevBags => [...prevBags, { x, y }]);
+// }
+
+// const bagsImages = bags.map((bag, i) => (
+//   <img
+//     key={i}
+//     src={chipsImg}
+//     className="bag"
+//     style={{ top: `${bag.y}px`, left: `${bag.x}px` }}
+//     alt="bag of lay's chips"
+//   />
+// ));
